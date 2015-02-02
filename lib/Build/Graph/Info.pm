0d921389a4f263178e9b4f717caed53f19fd62fa
Leon Timmermans <fawaka@gmail.com>
2015-02-02 17:35:18 +0100

    Rip Moo out of Info

--- lib/Build/Graph/Info.pm
+++ lib/Build/Graph/Info.pm
@@ -1,15 +1,27 @@
 package Build::Graph::Info;
-use Moo;
 
-has name => (
-	is       => 'ro',
-	required => 1,
-);
+use strict;
+use warnings;
 
-has arguments => (
-	is       => 'ro',
-	required => 1,
-);
+use Carp ();
+
+sub new {
+	my ($class, %args) = @_;
+	return bless {
+		name      => $args{name} || Carp::croak('No name given'),
+		arguments => $args{arguments},
+	}, $class;
+}
+
+sub name {
+	my $self = shift;
+	return $self->{name};
+}
+
+sub arguments {
+	my $self = shift;
+	return $self->{arguments};
+}
 
 1;
 
