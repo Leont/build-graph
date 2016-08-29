e0693a407f4700af8b7b814897984b500d15e371
Leon Timmermans <fawaka@gmail.com>
2016-08-29 13:51:42 +0200

    Allow action_list in substitutions too

--- lib/Build/Graph/Variable/Subst.pm
+++ lib/Build/Graph/Variable/Subst.pm
@@ -13,11 +13,29 @@ sub new {
 	my $self = $class->SUPER::new(%args);
 	$self->{name}              = $args{name}            || Carp::croak('No name given');
 	@{ $self->{trans}        } = @{ $args{trans}        || Carp::croak('No trans given')  };
-	@{ $self->{action}       } = @{ $args{action}       }  if $args{action};
+	if ($args{action_list}) {
+		@{ $self->{actions} } = map { [@$_] } @{ $args{action_list} };
+	}
+	elsif ($args{action}) {
+		@{ $self->{actions} } = [ @{ $args{action} } ];
+	}
 	@{ $self->{dependencies} } = @{ $args{dependencies} }  if $args{dependencies};
 	$self->{graph}             = $args{graph}           || Carp::croak('No graph given');
 	Scalar::Util::weaken($self->{graph});
 	return $self;
+}
+
+sub _serialize_actions {
+	my $self = shift;
+	if ($self->{actions}) {
+		if (@{ $self->{actions} } == 1) {
+			return action => [ @{ $self->{actions}[0] } ];
+		}
+		else {
+			return action_list => [ map { [@$_] } @{ $self->{actions} } ];
+		}
+	}
+	return;
 }
 
 sub process {
@@ -28,7 +46,7 @@ sub process {
 	my $commandset = $self->{graph}->lookup_commandset($commandset_name) or Carp::croak("No such commandset $commandset_name");
 	my $target = $commandset->get_transformation($subcommand)->(@arguments);
 
-	$self->{graph}->add_file($target, dependencies => [ $source, @{ $self->{dependencies} || [] } ], action => $self->{action}) if $self->{action};
+	$self->{graph}->add_file($target, dependencies => [ $source, @{ $self->{dependencies} || [] } ], $self->_serialize_actions);
 	$self->add_entries($target);
 	return;
 }
@@ -37,6 +55,10 @@ sub to_hashref {
 	my $self = shift;
 	my $ret  = $self->SUPER::to_hashref;
 	@{ $ret->{trans} } = @{ $self->{trans} };
+	if ($self->{actions}) {
+		my ($key, $value) = $self->_serialize_actions;
+		$ret->{$key} = $value;
+	}
 	@{ $ret->{action} } = @{ $self->{action} } if $self->{action};
 	@{ $ret->{dependencies} } = @{ $self->{dependencies} } if $self->{dependencies};
 	return $ret;
